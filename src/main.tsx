ee823429420e84099ec8be4f6ddc466c48551863
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-13 16:28:10 -0300

    feat: add shadcn template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import '@/index.css'
+import router from '@/router'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
+  </React.StrictMode>
 )
--- src/pages/produto/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Table } from "@/components/ui/table"
-
-export const ProdutosPage = () => {
-  return (<Table />)
-}
