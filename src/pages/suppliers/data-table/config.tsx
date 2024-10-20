1c2625144f761099c411854454288ddbdd8cd3b6
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-20 15:44:19 -0300

    refactor: supplier data table config

--- src/pages/suppliers/data-table/config.tsx
+++ src/pages/suppliers/data-table/config.tsx
@@ -5,7 +5,7 @@ import { DataTableRowActions, DropdownOption } from '@/components/data-table/dat
 import { Checkbox } from '@/components/ui/checkbox'
 
 import { DataTableToolbarConfig } from '@/components/data-table/data-table-toolbar'
-import { ProductSchema, productSchema } from './schema'
+import { SupplierSchema, supplierSchema } from './schema'
 
 export const rowActionsOptions: DropdownOption[] = [
   {
@@ -23,7 +23,7 @@ export const rowActionsOptions: DropdownOption[] = [
   },
 ]
 
-export const columns: ColumnDef<ProductSchema>[] = [
+export const columns: ColumnDef<SupplierSchema>[] = [
   {
     id: 'select',
     header: ({ table }) => (
@@ -76,15 +76,33 @@ export const columns: ColumnDef<ProductSchema>[] = [
     }
   },
   {
-    accessorKey: 'price',
+    accessorKey: 'contact',
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title='Preço' />
+      <DataTableColumnHeader column={column} title='Contato' />
     ),
     cell: ({ row }) => {
       return (
         <div className='flex space-x-2'>
           <span className='max-w-32 truncate font-medium sm:max-w-72 md:max-w-[31rem]'>
-            {row.getValue('price')}
+            {row.getValue('contact')}
+          </span>
+        </div>
+      )
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    }
+  },
+  {
+    accessorKey: 'address',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title='Endereço' />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className='flex space-x-2'>
+          <span className='max-w-32 truncate font-medium sm:max-w-72 md:max-w-[31rem]'>
+            {row.getValue('address')}
           </span>
         </div>
       )
@@ -95,29 +113,12 @@ export const columns: ColumnDef<ProductSchema>[] = [
   },
   {
     id: 'actions',
-    cell: ({ row }) => <DataTableRowActions row={row} schema={productSchema} options={rowActionsOptions} />,
+    cell: ({ row }) => <DataTableRowActions row={row} schema={supplierSchema} options={rowActionsOptions} />,
   },
 ]
 
 export const toolbar: DataTableToolbarConfig = {
-  placeholder: 'Filtrar produtos...',
+  placeholder: 'Filtrar fornecedores...',
   searchKey: 'name',
-  filters: [{
-    key: 'price',
-    title: 'Preço',
-    options: [
-      {
-        label: '< 50',
-        value: '50'
-      },
-      {
-        label: '< 100',
-        value: '100'
-      },
-      {
-        label: '< 300',
-        value: '300'
-      }
-    ]
-  }]
+  filters: []
 }
