e473137c32a4427503eb187778bf46bfe86b1244
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-20 21:33:13 -0300

    feat: add clients page

---
+++ src/pages/clients/add.tsx
@@ -0,0 +1,103 @@
+import { Button } from '@/components/custom/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { toast } from '@/components/ui/use-toast'
+import { BaseTemplate } from '@/template/Base'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { defaultValues, supplierFormSchema, SupplierFormValues } from './form-schema'
+
+export default function AddProductPage() {
+  const form = useForm<SupplierFormValues>({
+    resolver: zodResolver(supplierFormSchema),
+    defaultValues,
+    mode: 'onChange',
+  })
+
+  function onSubmit(data: SupplierFormValues) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  return (
+    <BaseTemplate>
+      <div className='mb-2 flex items-center justify-between space-y-2'>
+        <div className='flex w-full justify-between'>
+          <h2 className='text-2xl font-bold tracking-tight'>Novo fornecedor</h2>
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder='shadcn' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='contact'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contato</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder='+55 (45) 98765-4321'
+                    className='resize-none'
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Informações para contato do fornecedor
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='address'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Endereço</FormLabel>
+                <FormControl>
+                  <Input
+                    className='shadcn'
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Endereço do fornecedor
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Save</Button>
+        </form>
+      </Form>
+    </BaseTemplate>
+  )
+}
