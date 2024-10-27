a7c23ef87e4f0f2060a8808fb03951dc4ab322b9
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-27 19:45:10 -0300

    feat: implement auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import router from '@/router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
+    </AuthProvider>
   </React.StrictMode>
 )
