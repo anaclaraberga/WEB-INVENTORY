50cab042bf03c1481e8f35149a5beeee7a3e85d6
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-28 12:55:55 -0300

    refactor: implemented custom alert dialong

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,5 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/custom/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,15 +10,33 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { useAuth } from '@/hooks/use-auth'
+import { StringUtils } from '@/utils/StringUtils'
+import { useState } from 'react'
+import { CustomAlertDialog } from './custom/alert-dialog'
 
 export function UserNav() {
+  const { user, logout } = useAuth()
+  const [open, setOpen] = useState(false)
+
+  const handleLogout = () => {
+    logout()
+  }
+
   return (
     <DropdownMenu>
+      <CustomAlertDialog
+        open={open}
+        onClose={() => setOpen(false)}
+        onConfirm={handleLogout}
+        title='Tem certeza que deseja sair?'
+        description='Ao finalizar sua sessão, toda alteração não salva será perdida permanentemente'
+      />
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
           <Avatar className='h-8 w-8'>
             <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-            <AvatarFallback>SN</AvatarFallback>
+            <AvatarFallback>{StringUtils.getNameInitials(user?.name ?? 'Guilherme')}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
@@ -27,7 +45,7 @@ export function UserNav() {
           <div className='flex flex-col space-y-1'>
             <p className='text-sm font-medium leading-none'>satnaing</p>
             <p className='text-xs leading-none text-muted-foreground'>
-              satnaingdev@gmail.com
+              {user?.email}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -38,17 +56,12 @@ export function UserNav() {
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
             Settings
             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
           </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem className='text-red-500 !hover:text-red-500' onClick={() => setOpen(true)}>
           Log out
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
