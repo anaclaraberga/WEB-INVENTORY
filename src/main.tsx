cb2dbc101e14decf833b46ace8c759d4ff15fcb7
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-30 11:25:11 -0300

    refactor: changed routes to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import '@/index.css'
-import router from '@/router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import AppRouter from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-        <RouterProvider router={router} />
+        <AppRouter />
         <Toaster />
       </ThemeProvider>
     </AuthProvider>
--- src/router.tsx
+++ src/router.tsx
@@ -1,191 +1,72 @@
 import ProtectedRoute from '@/middlewares/ProtectedRoute.tsx';
-import Clients from '@/pages/admin/clients';
-import AddClientPage from '@/pages/admin/clients/add';
-import Dashboard from '@/pages/admin/dashboard';
-import Products from '@/pages/admin/products';
-import AddProductPage from '@/pages/admin/products/add.tsx';
-import Suppliers from '@/pages/admin/suppliers';
-import AddSupplierPage from '@/pages/admin/suppliers/add.tsx';
-import Apps from '@/pages/apps';
-import Chats from '@/pages/chats';
-import ExtraComponents from '@/pages/extra-components';
-import Tasks from '@/pages/tasks';
-import { Settings, Users } from 'lucide-react';
-import { createBrowserRouter } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from 'react-router-dom';
 import GeneralError from './pages/errors/general-error';
 import MaintenanceError from './pages/errors/maintenance-error';
 import NotFoundError from './pages/errors/not-found-error';
 import UnauthorizedError from './pages/errors/unauthorized-error.tsx';
-import Account from './pages/settings/account';
-import Profile from './pages/settings/profile';
+import Settings from './pages/settings/index.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/sign-in',
-    lazy: async () => ({
-      Component: (await import('./pages/auth/sign-in')).default,
-    }),
-  },
-  {
-    path: '/sign-up',
-    lazy: async () => ({
-      Component: (await import('./pages/auth/sign-up')).default,
-    }),
-  },
-  {
-    path: '/forgot-password',
-    lazy: async () => ({
-      Component: (await import('./pages/auth/forgot-password')).default,
-    }),
-  },
-  {
-    path: '/',
-    lazy: async () => {
-      const AppShell = await import('./components/app-shell');
-      return { Component: AppShell.default };
-    },
-    errorElement: <GeneralError />,
-    children: [
-      {
-        path: 'admin',
-        lazy: async () => {
-          const AppShell = await import('./components/app-shell');
-          return { Component: AppShell.default };
-        },
-        errorElement: <GeneralError />,
-        children: [
-          {
-            index: true,
-            element: (
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'tasks',
-            element: (
-              <ProtectedRoute>
-                <Tasks />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'products',
-            element: (
-              <ProtectedRoute>
-                <Products />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'product/:id',
-            element: (
-              <ProtectedRoute>
-                <AddProductPage />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'suppliers',
-            element: (
-              <ProtectedRoute>
-                <Suppliers />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'suppliers/:id',
-            element: (
-              <ProtectedRoute>
-                <AddSupplierPage />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'clients/:id',
-            element: (
-              <ProtectedRoute>
-                <AddClientPage />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'clients/',
-            element: (
-              <ProtectedRoute>
-                <Clients />
-              </ProtectedRoute>
-            ),
-          },
-        ]
-      },
-      {
-        path: 'general',
-        element: (
-          <ProtectedRoute>
-            <Chats />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'apps',
-        element: (
-          <ProtectedRoute>
-            <Apps />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'users',
-        element: (
-          <ProtectedRoute>
-            <Users />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'extra-components',
-        element: (
-          <ProtectedRoute>
-            <ExtraComponents />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'settings',
-        errorElement: <GeneralError />,
-        element: (
-          <ProtectedRoute>
-            <Settings />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            index: true,
-            element: (
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            ),
-          },
-          {
-            path: 'account',
-            element: (
-              <ProtectedRoute>
-                <Account />
-              </ProtectedRoute>
-            ),
-          },
-        ],
-      },
-    ],
-  },
-  { path: '/500', Component: GeneralError },
-  { path: '/404', Component: NotFoundError },
-  { path: '/503', Component: MaintenanceError },
-  { path: '/401', Component: UnauthorizedError },
-  { path: '*', Component: NotFoundError },
-]);
+// Lazy load components
+const SignIn = lazy(() => import('./pages/auth/sign-in.tsx'));
+const SignUp = lazy(() => import('./pages/auth/sign-up.tsx'));
+const ForgotPassword = lazy(() => import('./pages/auth/forgot-password'));
+const AppShell = lazy(() => import('./components/app-shell'));
+const LandingPage = lazy(() => import('./pages/landing-page/index.tsx'));
+const Dashboard = lazy(() => import('@/pages/admin/dashboard'));
+const Tasks = lazy(() => import('@/pages/tasks'));
+const Products = lazy(() => import('@/pages/admin/products'));
+const AddProductPage = lazy(() => import('@/pages/admin/products/add.tsx'));
+const Suppliers = lazy(() => import('@/pages/admin/suppliers'));
+const AddSupplierPage = lazy(() => import('@/pages/admin/suppliers/add.tsx'));
+const Clients = lazy(() => import('@/pages/admin/clients'));
+const AddClientPage = lazy(() => import('@/pages/admin/clients/add'));
+const Apps = lazy(() => import('@/pages/apps'));
+const Chats = lazy(() => import('@/pages/chats'));
+const ExtraComponents = lazy(() => import('@/pages/extra-components'));
+const Account = lazy(() => import('./pages/settings/account'));
+const Profile = lazy(() => import('./pages/settings/profile'));
 
-export default router;
+const AppRouter = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/">
+          <Route index element={<LandingPage />} />
+          <Route path="admin" element={<AppShell />}>
+            <Route index element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+            <Route path="tasks" element={<ProtectedRoute><Tasks /></ProtectedRoute>} />
+            <Route path="products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+            <Route path="product/:id" element={<ProtectedRoute><AddProductPage /></ProtectedRoute>} />
+            <Route path="suppliers" element={<ProtectedRoute><Suppliers /></ProtectedRoute>} />
+            <Route path="suppliers/:id" element={<ProtectedRoute><AddSupplierPage /></ProtectedRoute>} />
+            <Route path="clients" element={<ProtectedRoute><Clients /></ProtectedRoute>} />
+            <Route path="clients/:id" element={<ProtectedRoute><AddClientPage /></ProtectedRoute>} />
+          </Route>
+          <Route path="general" element={<ProtectedRoute><Chats /></ProtectedRoute>} />
+          <Route path="apps" element={<ProtectedRoute><Apps /></ProtectedRoute>} />
+          {/* <Route path="users" element={<ProtectedRoute><Users /></ProtectedRoute>} /> */}
+          <Route path="extra-components" element={<ProtectedRoute><ExtraComponents /></ProtectedRoute>} />
+          <Route path="settings" element={<ProtectedRoute><Settings /></ProtectedRoute>}>
+            <Route index element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
+          </Route>
+        </Route>
+        <Route path="/500" element={<GeneralError />} />
+        <Route path="/404" element={<NotFoundError />} />
+        <Route path="/503" element={<MaintenanceError />} />
+        <Route path="/401" element={<UnauthorizedError />} />
+        <Route path="*" element={<NotFoundError />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
+
+export default AppRouter;
