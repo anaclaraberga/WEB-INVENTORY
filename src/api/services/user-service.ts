c5d3f90188c15b297e1602670212a2964505257b
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-31 14:28:32 -0300

    feat: add user service

--- src/api/services/user-service.ts
+++ src/api/services/user-service.ts
@@ -1,9 +1,30 @@
+import { UserFormValues } from '@/pages/admin/users/form-schema'
 import apiClient from '../client/api'
+import { UserMapper, UserResponseDTO } from '../mappers/user-mapper'
 
 export const UserService = {
-  findAll: () => apiClient.get('/user'),
-  findById: (id) => apiClient.get(`/user/${id}`),
-  create: (data) => apiClient.post('/user', JSON.stringify(data)),
-  update: (id, data) => apiClient.post(`/user/${id}`, JSON.stringify(data)),
-  delete: (id) => apiClient.delete(`/user/${id}`),
+  findAll: async () => {
+    const response = await apiClient.get('/user')
+    return response.data.map((element: UserResponseDTO) => {
+      return UserMapper.toDomain(element)
+    })
+  },
+  findById: async (id: string | number) => {
+    const response = await apiClient.get(`/user/${id}`)
+    return UserMapper.toDomain(response.data)
+  },
+  create: async (data: UserFormValues) => {
+    const body = UserMapper.toRequest(data)
+    const response = await apiClient.post('/user', JSON.stringify(body))
+    return UserMapper.toDomain(response.data)
+  },
+  update: async (id: string | number, data: UserFormValues) => {
+    const body = UserMapper.toRequest(data)
+    const response = await apiClient.post(`/user/${id}`, JSON.stringify(body))
+    return UserMapper.toDomain(response.data)
+  },
+  delete: async (id: string | number) => {
+    const response = await apiClient.delete(`/user/${id}`)
+    return response.data
+  },
 }
