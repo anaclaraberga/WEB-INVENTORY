8d2d23d6ee97b23feadbab5b378df3e4a31918a4
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-31 14:55:57 -0300

    refactor: customer service

--- src/api/services/customer-service.ts
+++ src/api/services/customer-service.ts
@@ -1,31 +1,33 @@
 import { ClientFormValues } from '@/pages/admin/clients/form-schema'
 import apiClient from '../client/api'
-import { CustomerMapper } from '../mappers/customer-mapper'
+import { CustomerMapper, CustomerResponseDTO } from '../mappers/customer-mapper'
 
 export const CustomerService = {
   findAll: async () => {
-    const response = await apiClient.get('/supplier')
-    return CustomerMapper.toDomain(response.data)
+    const response = await apiClient.get('/customer')
+    return response.data.map((element: CustomerResponseDTO) => {
+      return CustomerMapper.toDomain(element)
+    })
   },
   findById: async (id: string | number) => {
-    const response = await apiClient.get(`/supplier/${id}`)
+    const response = await apiClient.get(`/customer/${id}`)
     return CustomerMapper.toDomain(response.data)
   },
   create: async (data: ClientFormValues) => {
     const body = CustomerMapper.toRequest(data)
-    const response = await apiClient.post('/supplier', JSON.stringify(body))
+    const response = await apiClient.post('/customer', JSON.stringify(body))
     return CustomerMapper.toDomain(response.data)
   },
   update: async (id: string | number, data: ClientFormValues) => {
     const body = CustomerMapper.toRequest(data)
     const response = await apiClient.post(
-      `/supplier/${id}`,
+      `/customer/${id}`,
       JSON.stringify(body)
     )
     return CustomerMapper.toDomain(response.data)
   },
   delete: async (id: string | number) => {
-    const response = await apiClient.delete(`/supplier/${id}`)
+    const response = await apiClient.delete(`/customer/${id}`)
     return response.data
   },
 }
