00104b387d0564d3a4f8c32c89924c8c2664a67a
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-31 14:28:18 -0300

    feat: add user mapper

---
+++ src/api/mappers/user-mapper.ts
@@ -0,0 +1,28 @@
+import { UserFormValues } from '@/pages/admin/users/form-schema'
+
+export interface UserResponseDTO {
+  id: string | number | null
+  name: string
+  email: string
+  password: string
+}
+
+export const UserMapper = {
+  toDomain: (response: UserResponseDTO): UserFormValues => {
+    return {
+      id: response.id as number,
+      name: response.name,
+      email: response.email,
+      password: response.password,
+      confirm: '',
+    }
+  },
+  toRequest: (domain: UserFormValues): UserResponseDTO => {
+    return {
+      id: domain.id as number,
+      name: domain.name,
+      email: domain.email,
+      password: domain.password,
+    }
+  },
+}
