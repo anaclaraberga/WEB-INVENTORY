c7fccc64651eab9e77bdeb6afa231e94bc96ec2e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-11-15 16:39:42 -0300

    feat: add supplier card

---
+++ src/components/supplier-card.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card"
+
+export interface SupplierCardProps extends HTMLAttributes<HTMLDivElement> {
+  name: string
+  description: string
+}
+
+export const SupplierCard = ({ name, description, ...props }: SupplierCardProps) => {
+
+  return (
+    <Card className={cn("w-[380px]", props.className)} {...props}>
+      <CardHeader className="grid gap-4 p-4">
+        <CardTitle>{name}</CardTitle>
+        {description && <CardDescription>{description}</CardDescription>}
+      </CardHeader>
+    </Card>
+  )
+}
