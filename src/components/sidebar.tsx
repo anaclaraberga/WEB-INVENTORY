6f497cba1624c38880b2ba3aeac5fb2c682916cc
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-11-23 22:54:02 -0300

    refactor: dashboard page

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,11 @@
 import { useAuth } from '@/hooks/use-auth'
 import { cn } from '@/lib/utils'
-import { ChevronLeft, Menu, X } from 'lucide-react'
+import { ChevronLeft, LayoutDashboardIcon, Menu, X } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { Button } from './custom/button'
 import { Layout } from './custom/layout'
 import Nav from './nav'
+import { UserNav } from './user-nav'
 
 interface SidebarProps extends React.HTMLAttributes<HTMLElement> {
   isCollapsed: boolean
@@ -47,40 +48,11 @@ export default function Sidebar({
           className='z-50 flex justify-between px-4 py-3 shadow-sm md:px-4'
         >
           <div className={`flex items-center ${!isCollapsed ? 'gap-2' : ''}`}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 256 256'
-              className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-8 w-8'}`}
-            >
-              <rect width='256' height='256' fill='none'></rect>
-              <line
-                x1='208'
-                y1='128'
-                x2='128'
-                y2='208'
-                fill='none'
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='16'
-              ></line>
-              <line
-                x1='192'
-                y1='40'
-                x2='40'
-                y2='192'
-                fill='none'
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='16'
-              ></line>
-              <span className='sr-only'>Website Name</span>
-            </svg>
+            <LayoutDashboardIcon />
             <div
               className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
             >
-              <span className='font-medium'>Shadcn Admin</span>
+              <span className='font-medium'>WebInventory</span>
               <span className='text-xs'>Vite + ShadcnUI</span>
             </div>
           </div>
@@ -119,6 +91,10 @@ export default function Sidebar({
             className={`h-5 w-5 ${isCollapsed ? 'rotate-180' : ''}`}
           />
         </Button>
+
+        <Layout.Header>
+          <UserNav/> Perfil
+        </Layout.Header>
       </Layout>
     </aside>
   )
